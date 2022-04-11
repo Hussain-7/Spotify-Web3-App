04078b59517f66bdd5117a08d25fdd72e5bf1a7f
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-04-11 07:15:04 +0500

    main page completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import { Tabs } from "antd";
+import { library } from "../helpers/albumList";
 const { TabPane } = Tabs;
+
 const Home = () => {
   return (
     <>
       <Tabs defaultActiveKey="1" centered>
         <TabPane tab="Featured" key="1">
-          Content of Tab Pane 1
+          <h1 className="featuredTitle">Today Is the Day</h1>
+          <div className="albums">
+            {library.map((album) => (
+              <Link to={`/album`} state={album} className="albumSelection">
+                <img
+                  src={album.image}
+                  alt="something"
+                  style={{ width: "150px", marginBottom: "10px" }}
+                />
+                <p>{album.title}</p>
+              </Link>
+            ))}
+          </div>
         </TabPane>
         <TabPane tab="Genres & Moods" key="2">
           Content of Tab Pane 2
