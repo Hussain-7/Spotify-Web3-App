c7f3ba38c841a2c6b06a275f542b11401e6c843c
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-04-11 06:23:07 +0500

    side bar completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Album from "./pages/Album";
 import "./App.css";
@@ -10,12 +10,24 @@ const { Header, Footer, Sider, Content } = Layout;
 const App = () => {
   return (
     <Layout>
-      <Sider width="300">
+      <Sider width="300" className="sideBar">
         {" "}
         <img src={Spotify} alt="" className="logo" />
         <div className="searchBar">
           <span>Search</span>
           <SearchOutlined style={{ fontSize: "30px" }} />
+        </div>
+        <Link to="/">
+          {" "}
+          <p>Home</p>
+        </Link>
+        <p>Your Music</p>
+        <div className="recentPlayed">
+          <p className="recentTitle">RECENTLY PLAYED</p>
+          <div className="install">
+            <span>Install App</span>
+            <DownCircleOutlined style={{ fontSize: "30px" }} />
+          </div>
         </div>
       </Sider>
 
