6c95d057dcf6ee61297245f2882548b2fe7811b9
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-04-17 04:24:09 +0500

    updated scripts

--- Albums/metadata.js
+++ Albums/metadata.js
@@ -1,20 +1,47 @@
 let fs = require("fs");
 let axios = require("axios");
 
-let songs = [];
-let durations = [];
+let songs = [
+  "Ava Max - Sweet but Psycho.mp3",
+  "Fast Five - Don Omar.mp3",
+  "Imagine Dragons - Bones.mp3",
+  "Olivia Rodrigo - happier.mp3",
+  "Glass Animals - Heat Waves.mp3",
+];
+let animation_url = [
+  "https://ipfs.moralis.io:2053/ipfs/QmdgYdsLnD6f1sPFkEE8Kb6BWbZKTZbrVsBSCgeysLfBiU/media/1",
+  "https://ipfs.moralis.io:2053/ipfs/QmdgYdsLnD6f1sPFkEE8Kb6BWbZKTZbrVsBSCgeysLfBiU/media/2",
+  "https://ipfs.moralis.io:2053/ipfs/QmefbQ7SkQMixLsXmpAWQyyPsuKiRjVExXcWowHzUjiQH5/media/3",
+  "https://ipfs.moralis.io:2053/ipfs/QmefbQ7SkQMixLsXmpAWQyyPsuKiRjVExXcWowHzUjiQH5/media/4",
+  "https://ipfs.moralis.io:2053/ipfs/QmfEoHvurf4LJPrD2sArw6jRt2hDRqFGJ6kBUbEKAeTYoC/media/5",
+];
+
+let durations = ["3:27", "3:27", "3:55", "2:45", "2:56"];
 let ipfsArray = [];
 
 for (let i = 0; i < songs.length; i++) {
+  console.log({
+    path: `metadata/${i}.json`,
+    content: {
+      image:
+        "https://ipfs.moralis.io:2053/ipfs/QmdgYdsLnD6f1sPFkEE8Kb6BWbZKTZbrVsBSCgeysLfBiU/media/0",
+      name: songs[i],
+      animation_url: animation_url[i],
+      duration: durations[i],
+      artist: "Hussain Rizvi",
+      year: "2021",
+    },
+  });
   ipfsArray.push({
     path: `metadata/${i}.json`,
     content: {
-      image: `ipfs://xxx`, //xxx = hash
+      image:
+        "https://ipfs.moralis.io:2053/ipfs/QmdgYdsLnD6f1sPFkEE8Kb6BWbZKTZbrVsBSCgeysLfBiU/media/0",
       name: songs[i],
-      animation_url: `ipfs://xxx/`, //xxx = hash
+      animation_url: animation_url[i],
       duration: durations[i],
-      artist: "",
-      year: "",
+      artist: "Hussain Rizvi",
+      year: "2021",
     },
   });
 }
@@ -22,7 +49,8 @@ for (let i = 0; i < songs.length; i++) {
 axios
   .post("https://deep-index.moralis.io/api/v2/ipfs/uploadFolder", ipfsArray, {
     headers: {
-      "X-API-KEY": "<API Key Here>",
+      "X-API-KEY":
+        "FmpB7bIckgJMbg6gOqCnrBpPHj2hd6WdlwcZ6wFoqCuWswR2t4f1dqy65jarTL2I",
       "Content-Type": "application/json",
       accept: "application/json",
     },
@@ -31,5 +59,24 @@ axios
     console.log(res.data);
   })
   .catch((error) => {
-    console.log(error);
+    // console.log(error);
   });
+
+// results
+const results = [
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmPt5dALzuWqEpnWEaM2fdGE9S7vGkgdJLr11hh4e8VNFL/metadata/0.json",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmPt5dALzuWqEpnWEaM2fdGE9S7vGkgdJLr11hh4e8VNFL/metadata/1.json",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmPt5dALzuWqEpnWEaM2fdGE9S7vGkgdJLr11hh4e8VNFL/metadata/2.json",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmPt5dALzuWqEpnWEaM2fdGE9S7vGkgdJLr11hh4e8VNFL/metadata/3.json",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmPt5dALzuWqEpnWEaM2fdGE9S7vGkgdJLr11hh4e8VNFL/metadata/4.json",
+  },
+];
--- Albums/music.js
+++ Albums/music.js
@@ -1,13 +1,24 @@
 let fs = require("fs");
 let axios = require("axios");
+const path = require("path");
 
 let media = [
+  "cover.jpg",
   "Ava Max - Sweet but Psycho.mp3",
   "Fast Five - Don Omar.mp3",
-  "Glass Animals - Heat Waves.mp3",
   "Imagine Dragons - Bones.mp3",
   "Olivia Rodrigo - happier.mp3",
+  "Glass Animals - Heat Waves.mp3",
 ];
+const readFileInFolder = async () => {
+  fs.readdir(path.resolve(__dirname, "export"), (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+      media.push(file);
+    }
+  });
+};
+await readFileInFolder();
 let ipfsArray = [];
 let promises = [];
 for (let i = 0; i < media.length; i++) {
@@ -24,6 +35,7 @@ for (let i = 0; i < media.length; i++) {
     })
   );
 }
+console.log(promises);
 Promise.all(promises).then(() => {
   axios
     .post("https://deep-index.moralis.io/api/v2/ipfs/uploadFolder", ipfsArray, {
@@ -38,6 +50,28 @@ Promise.all(promises).then(() => {
       console.log(res.data);
     })
     .catch((error) => {
-      console.log(error);
+      console.log("error in axios");
     });
 });
+
+// Results
+const ipfs = [
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmdgYdsLnD6f1sPFkEE8Kb6BWbZKTZbrVsBSCgeysLfBiU/media/0",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmdgYdsLnD6f1sPFkEE8Kb6BWbZKTZbrVsBSCgeysLfBiU/media/1",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmdgYdsLnD6f1sPFkEE8Kb6BWbZKTZbrVsBSCgeysLfBiU/media/2",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmefbQ7SkQMixLsXmpAWQyyPsuKiRjVExXcWowHzUjiQH5/media/3",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmefbQ7SkQMixLsXmpAWQyyPsuKiRjVExXcWowHzUjiQH5/media/4",
+  },
+  {
+    path: "https://ipfs.moralis.io:2053/ipfs/QmfEoHvurf4LJPrD2sArw6jRt2hDRqFGJ6kBUbEKAeTYoC/media/5",
+  },
+];
